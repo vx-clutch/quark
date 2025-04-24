7e92c49995147360c6413df18dc0471cf97602fc
owestn0480 <owestn0480@rcs-k12.us>
2025-04-24 08:15:20 -0400

    save

--- vlibc.h
+++ vlibc.h
@@ -15,12 +15,14 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
 // Strings
 typedef struct {
-  char *value;
-  int length;
+  char *to_str;
+  int len;
 }
+
 #ifndef VLIBC_SHORT
 vlibc_string_t;
 #else
@@ -29,8 +31,10 @@ string_t;
 
 #ifndef VLIBC_SHORT
 #define _vlibc_string_t vlibc_string_t
+#define s(s) vlibc_new_string(s)
 #else
 #define _vlibc_string_t string_t
+#define s(s) new_string(s)
 #endif
 
 static _vlibc_string_t mixin(new_string)(char *src) {
@@ -43,21 +47,32 @@ static _vlibc_string_t mixin(new_string)(char *src) {
     break;
   }
   _vlibc_string_t buf;
-  buf.value = src;
-  buf.length = length;
+  buf.to_str = src;
+  buf.len = length;
   return buf;
 }
 
-static _vlibc_string_t mixin(strcat)(_vlibc_string_t dest, _vlibc_string_t src) {
-  char *d = dest.value;
-  while (*d)
-    d++;
-  while ((*d++ = *src.value++))
-    ;
-  return (_vlibc_string_t){
-      .value = dest.value,
-      .length = dest.length + src.length,
-  };
+#ifndef VLIBC_SHORT
+#define _strcpy(d, s) vlibc_strcpy(d, s)
+#else
+#define _strcpy(d, s) strcpy(d, s)
+#endif
+
+static _vlibc_string_t mixin(strcpy)(_vlibc_string_t dest,
+                                     _vlibc_string_t src) {
+  dest.to_str = src.to_str;
+  dest.len = src.len;
+  return dest;
+}
+
+#ifndef VLIBC_SHORT
+#define _strcat(d, s) vlibc_strcat(d, s)
+#else
+#define _strcat(d, s) strcat(d, s)
+#endif
+
+static _vlibc_string_t mixin(strcat)(_vlibc_string_t dest,
+                                     _vlibc_string_t src) {
 }
 
 // Logging
@@ -68,35 +83,54 @@ static const char *VLIBC_GREEN = "[1;32m";
 static const char *VLIBC_PURPLE = "[0;95m";
 static const char *VLIBC_WHITE = "[0;97m";
 
+// __level_t mappings
+#define OK _ok,
+#define ERR _error,
+#define WARN _warn,
+#define INFO _info,
+#define FATAL _fatal,
+
 typedef enum {
-  ok,
-  error,
-  warn,
-  info,
-  fatal,
+  _ok,
+  _error,
+  _warn,
+  _info,
+  _fatal,
 } __level_t;
 
-#define vlibc_case(message, level_color)                                       \
+#define _vlibc_case(message, level_color)                                       \
   case message:                                                                \
-    printf("%s%s%s: %s" #message "%s: ", VLIBC_WHITE, VLIBC_PACKAGE,           \
-           VLIBC_RESET, level_color, VLIBC_RESET);                             \
+    fprintf(stderr, "%s%s%s: %s" #message "%s: ", VLIBC_WHITE, VLIBC_PACKAGE,  \
+            VLIBC_RESET, level_color, VLIBC_RESET);                            \
     break;
+
+#ifndef VLIBC_SHORT
+#define _logf(l, s, ...) vlibc_logf(l, s, __VA_ARGS__)
+#else
+#define _logf(l, s, ...) logf(l, s, __VA_ARGS__)
+#endif
 
 static int mixin(logf)(__level_t level, char *src, ...) {
   va_list args;
   va_start(args, src);
   switch (level) {
-    vlibc_case(ok, VLIBC_GREEN);
-    vlibc_case(error, VLIBC_RED);
-    vlibc_case(warn, VLIBC_YELLOW);
-    vlibc_case(info, VLIBC_PURPLE);
-    vlibc_case(fatal, VLIBC_RED);
+    _vlibc_case(_ok, VLIBC_GREEN);
+    _vlibc_case(_error, VLIBC_RED);
+    _vlibc_case(_warn, VLIBC_YELLOW);
+    _vlibc_case(_info, VLIBC_PURPLE);
+    _vlibc_case(_fatal, VLIBC_RED);
   }
-  vprintf(src, args);
+  fvprintf(stderr, src, args);
   printf("\n");
   va_end(args);
   return 0;
 }
 
+#ifndef VLIBC_SHORT
+#undef s
+#endif
+
 #undef mixin
+#undef _mixin_concat
+#undef _vlibc_case
 #endif
