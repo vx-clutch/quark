20c10c197d51984f63875531fcd40e9316d62cbe
vx-clutch <owestness@gmail.com>
2025-04-23 22:16:33 -0400

    more testing needs to be done

--- vlibc.h
+++ vlibc.h
@@ -1,6 +1,13 @@
 #ifndef VLIBC_H
 #define VLIBC_H
 
+#ifndef VLIBC_SHORT
+#define _mixin_concat(a, b) a##b
+#define mixin(name) _mixin_concat(vlibc_, name)
+#else
+#define mixin(name) name
+#endif
+
 #ifndef VLIBC_PACKAGE
 #error "macro `VLIBC_PACKAGE` not defined. Store the name of the package here."
 #endif
@@ -13,9 +20,20 @@
 typedef struct {
   char *value;
   int length;
-} vlibc_string_t;
+}
+#ifndef VLIBC_SHORT
+vlibc_string_t;
+#else
+string_t;
+#endif
 
-static vlibc_string_t vlibc_new_string(char *src) {
+#ifndef VLIBC_SHORT
+#define _vlibc_string_t vlibc_string_t
+#else
+#define _vlibc_string_t string_t
+#endif
+
+static _vlibc_string_t mixin(new_string)(char *src) {
   int length = 0;
   for (;;) {
     if (src[length] != '\0') {
@@ -24,19 +42,19 @@ static vlibc_string_t vlibc_new_string(char *src) {
     }
     break;
   }
-  vlibc_string_t buf;
+  _vlibc_string_t buf;
   buf.value = src;
   buf.length = length;
   return buf;
 }
 
-static vlibc_string_t vlibc_strcat(vlibc_string_t dest, vlibc_string_t src) {
+static _vlibc_string_t mixin(strcat)(_vlibc_string_t dest, _vlibc_string_t src) {
   char *d = dest.value;
   while (*d)
     d++;
   while ((*d++ = *src.value++))
     ;
-  return (vlibc_string_t){
+  return (_vlibc_string_t){
       .value = dest.value,
       .length = dest.length + src.length,
   };
@@ -64,7 +82,7 @@ typedef enum {
            VLIBC_RESET, level_color, VLIBC_RESET);                             \
     break;
 
-static int vlibc_logf(__level_t level, char *src, ...) {
+static int mixin(logf)(__level_t level, char *src, ...) {
   va_list args;
   va_start(args, src);
   switch (level) {
@@ -80,4 +98,5 @@ static int vlibc_logf(__level_t level, char *src, ...) {
   return 0;
 }
 
+#undef mixin
 #endif
