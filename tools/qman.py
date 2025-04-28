da96bbc4f7bfa6fe38da30931c7009683100efd5
vx-clutch <owestness@gmail.com>
2025-04-28 16:43:40 -0400

    dynamic man

--- tools/qman.py
+++ tools/qman.py
@@ -1,7 +1,53 @@
 import sys
 import webbrowser
 import os
+from html.parser import HTMLParser
 
+class QuarkHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.in_target = False
+        self.current_tag = None
+        self.entries = {}
+        self.current_title = None
+        self.current_content = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag in ("h3", "h4"):
+            self.in_target = True
+            self.current_tag = tag
+            self.current_content = []
+
+    def handle_endtag(self, tag):
+        if tag in ("h3", "h4") and self.in_target:
+            title = ''.join(self.current_content).strip()
+            self.entries[title] = ""
+            self.current_title = title
+            self.in_target = False
+        elif self.current_title and tag in ("p", "div", "ul", "ol", "pre"):
+            pass  # Continue collecting content if needed
+
+    def handle_data(self, data):
+        if self.in_target:
+            self.current_content.append(data)
+        elif self.current_title:
+            # Collect content after heading
+            self.entries[self.current_title] += data.strip() + " "
+
+def quark_doc():
+    html_path = os.path.join(os.getcwd(), 'index.html')
+    if not os.path.isfile(html_path):
+        print(f"Could not find {html_path}")
+        exit(0)
+    original_stderr = os.dup(2)
+    devnull = os.open(os.devnull, os.O_WRONLY)
+    try:
+        os.dup2(devnull, 2)
+        webbrowser.open(f'file://{html_path}')
+    finally:
+        os.dup2(original_stderr, 2)
+        os.close(devnull)
+    print("Opening docs in your browser")
 
 def quark_help():
     print("Usage: qman [argument] ...\n")
@@ -11,36 +57,43 @@ def quark_help():
     print("  help\tPrint this text and exit.")
     exit(0)
 
-
-def quark_doc():
-    html_path = os.path.join(os.getcwd(), 'doc', 'index.html')
-
-    if not os.path.isfile(html_path):
-        print(html_path)
-        exit(0)
-
-    # Open the HTML file in the default web browser
-    webbrowser.open(f'file://{html_path}')
-
-
 def g(src): return f"\n\n#include \"quarks.h\"\n{src}"
-
 
 def quark_man(page):
     if page[:2] == "q_":
-        page = page[2:]  # remove prefix
-    match page:
-        case "string_t": print("string_t is a string used by the quark library. It has two fields `to_str` and `len`" + g("string_t src = new_string(\"Hello, World!\");"))
-        case "logf": print("logf() is a function used for general purpose printing to the user. It takes at least two arguments. A level, see level_t, and a format. It can also have a variable ammount of extra arguemnts as long as format has format specifier. All output from logf() is sent to stderr" + g("logf(OK \"hello, World!\");"))
-        case "level_t": print("level_t is a internal type used in quark. It is intented that the uses called it through the macros of OK, ERR, WARN, INFO, and FATAL." + g("logf(OK \"Hello, World!\"); "))
-        case _: print(f"No manual entry for {page}")
+        page = page[2:]  # remove prefix 'q_'
+    if page.endswith("()"):
+        page = page[:-2]  # remove parens from function
 
+    html_path = os.path.join(os.getcwd(), 'index.html')
+    if not os.path.isfile(html_path):
+        print(f"Could not find {html_path}")
+        exit(1)
+
+    with open(html_path, 'r', encoding='utf-8') as f:
+        html_data = f.read()
+
+    parser = QuarkHTMLParser()
+    parser.feed(html_data)
+
+    for heading, content in parser.entries.items():
+        if heading.startswith("q_"):
+            normalized = heading[2:].lower().replace("()", "").replace(" ", "").replace("_", "")
+            target = page.lower().replace("()", "").replace(" ", "").replace("_", "")
+            if normalized == target:
+                print(f"QUARK\n\t{heading} - {' '.join(content.strip().split())}")
+                return
+
+    print(f"No manual entry for {page}")
 
 if len(sys.argv) < 2:
     quark_help()
 if sys.argv[1] == "doc":
     quark_doc()
 elif sys.argv[1] == "man":
+    if len(sys.argv) < 3:
+        print("error: No page specified for man command.")
+        exit(1)
     quark_man(sys.argv[2])
 elif sys.argv[1] == "help":
     quark_help()
