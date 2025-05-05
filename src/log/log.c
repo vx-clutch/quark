6e1c228892c6981e993bb0495dbab8ab2e64220a
vx-clutch <owestness@gmail.com>
2025-05-05 15:49:06 -0400

    save

--- src/log/log.c
+++ src/log/log.c
@@ -2,31 +2,6 @@
 #include <stdio.h>
 #include "../../include/log.h"
 
-#ifndef QUARK_PACKAGE
-#define QUARK_PACKAGE "quarks"
-#endif
-
-static const char *QUARK_RESET = "[0m";
-static const char *QUARK_RED = "[1;91m";
-static const char *QUARK_YELLOW = "[0;33m";
-static const char *QUARK_GREEN = "[1;32m";
-static const char *QUARK_PURPLE = "[0;95m";
-static const char *QUARK_WHITE = "[0;97m";
-
-#define OK ok,
-#define ERR error,
-#define WARN warn,
-#define INFO info,
-#define FATAL fatal,
-
-typedef enum {
-  ok,
-  error,
-  warn,
-  info,
-  fatal,
-} q_level_t;
-
 #define quark_case(message, level_color)                                       \
   case message:                                                                \
     fprintf(stderr, "%s%s%s: %s" #message "%s: ", QUARK_WHITE, QUARK_PACKAGE,  \
